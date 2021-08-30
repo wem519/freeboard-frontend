f07726ec482151ad10ab929aa93cf42ed9cfc910
wem519 <wem519@gmail.com>
2021-08-30 14:58:22 +0900

    New folder

---
+++ freeboard_frontend/pages/boards/new/index.js
@@ -0,0 +1,13 @@
+import { MyDiv } from "../../../../class/styles/Example";
+
+export default function BoardsNewPage(){
+
+    return(
+        <div>  
+            <div>게시글 등록 화면</div>
+            <h1>게시물 등록</h1>
+            <div>제목</div>
+            <input type="text" placeholder="제목을 작성해주세요"/>
+        </div>
+    )
+}
