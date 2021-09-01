bb4fab4ceddb042828402586bed274213d964f36
wem519 <wem519@gmail.com>
2021-09-01 09:24:35 +0900

    1-1 알고리즘

---
+++ programmers/1week.js
@@ -0,0 +1,26 @@
+// 1. 배열의 선언과 할당
+
+let fruits = ["사과", "바나나", "파인애플"]
+
+// 2.배열의 기능
+
+let name = "철수"
+
+//  3. 객체의 선언과 할당
+
+let students = {}
+students.name = "철수"
+
+// 4.객체의 키 & 값 추가
+
+const student = {
+	name: "철수",
+	age: 8,
+};
+
+const school = {
+	name: "다람쥐초등학교",
+	teacher: "다람이",
+}
+
+student.school = school
