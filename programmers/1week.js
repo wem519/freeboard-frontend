0346642e3365c8ceee177a03b1093df44c449fcb
wem519 <wem519@gmail.com>
2021-09-03 10:41:15 +0900

    4일차 알고리즘

--- programmers/1week.js
+++ programmers/1week.js
@@ -133,3 +133,42 @@ function bigNum(str) {
 	}
 	console.log(max)
 	}
+
+//14. 점수에 따른 등급
+
+function grade(score) {
+	if (score > 100 || score < 0) {
+		console.log("잘못된 점수입니다.");
+	} else if (score >= 90) {
+		console.log("A");
+	} else if (score >= 80) {
+		console.log("B");
+	} else if (score >= 70) {
+		console.log("C");
+	} else if (score >= 60) {
+		console.log("D");
+	} else if (score <= 59) {
+		console.log("F");
+	}
+}
+
+//마이페이지
+
+let count = 0;
+let money = 0;
+let grade = "";
+for(let i=0; i<myShopping.length; i++){
+	if (myShopping[i].category === "의류"){
+		count= count++;
+		money = money + myShopping[i].price;
+	}
+
+if (count>=5){
+	grade = "gold";
+}else if (count >=3){
+	grade = "silver";
+}else if (count>=0){
+	grade = "bronze"
+}
+return("의류를 구매한 횟수는 총" + count + "회 금액은" + money + "등급은" + grade + "입니다.")
+}
