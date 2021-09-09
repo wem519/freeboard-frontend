9352423a7fd02031ecb6cbee1bc623c5928a8539
wem519 <wem519@gmail.com>
2021-09-09 13:05:11 +0900

    2주차 3일 quiz

---
+++ class/pages/quiz/08-product-detail/[id]/edit/index.js
@@ -0,0 +1,5 @@
+import BoardWrite from '../../../../../src/components/units/quiz/product/ProductSeller.container'
+
+export default function BoardEditPage(){
+    return <BoardWrite isEdit={true} />
+}
---
+++ class/pages/quiz/08-product/index.js
@@ -0,0 +1,6 @@
+import ProductSeller from "../../../src/components/units/quiz/product/ProductSeller.container"
+
+export default function ProductNewPage(){
+
+    return<ProductSeller isEdit={false}/>
+}
