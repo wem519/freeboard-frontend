37beb027280bd9d446a86f756de132a471909857
wem519 <wem519@gmail.com>
2021-09-15 10:02:55 +0900

    게시판 댓글 등록

--- freeboard_frontend/src/components/units/board/BoardComment/write/BoardCommentWrite.container.js
+++ freeboard_frontend/src/components/units/board/BoardComment/write/BoardCommentWrite.container.js
@@ -1,5 +1,60 @@
 import BoardCommentWriteUI from "./BoardCommentWrite.presenter";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { CREATE_BOARD_COMMENT } from "./BoardCommentWrite.queries";
+import { useMutation } from "@apollo/client";
 
-export default function BoardCommentWrite() {
-  return <BoardCommentWriteUI />;
+export default function BoardCommentWrite(props) {
+  const router = useRouter();
+  const [myWriter, setMyWriter] = useState("");
+  const [myPassWord, setMyPassword] = useState("");
+  const [myContents, setMyContents] = useState("");
+  const [myStar, setMyStar] = useState(0);
+
+  const [createBoardComment] = useMutation(CREATE_BOARD_COMMENT);
+
+  function onChangeMyWiter(event) {
+    setMyWriter(event.target.value);
+  }
+  function onChangeMyPassword(event) {
+    setMyPassword(event.target.value);
+  }
+
+  function onChangeMyContents(event) {
+    setMyContents(event.target.value);
+  }
+  function onChangeStar(value) {
+    setMyStar(value);
+  }
+
+  async function onClickWrite() {
+    try {
+      const result = await createBoardComment({
+        variables: {
+          createBoardCommentInput: {
+            writer: myWriter,
+            password: myPassWord,
+            contents: myContents,
+            rating: myStar,
+          },
+          boardId: router.query.read,
+        },
+      });
+      console.log(result.data.createBoardComment);
+    } catch (error) {
+      alert(error);
+    }
+  }
+
+  return (
+    <>
+      <BoardCommentWriteUI
+        onChangeMyWiter={onChangeMyWiter}
+        onChangeMyPassword={onChangeMyPassword}
+        onChangeMyContents={onChangeMyContents}
+        onChangeStar={onChangeStar}
+        onClickWrite={onClickWrite}
+      />
+    </>
+  );
 }
--- freeboard_frontend/src/components/units/board/BoardComment/write/BoardCommentWrite.presenter.js
+++ freeboard_frontend/src/components/units/board/BoardComment/write/BoardCommentWrite.presenter.js
@@ -1,6 +1,7 @@
 import {
   Wrapper,
   CommentIcon,
+  Star,
   InputWrapper,
   Input,
   ContentsWrapper,
@@ -11,21 +12,29 @@ import {
   Line,
 } from "./BoardCommentWrite.styles";
 
-export default function BoardCommentWriteUI() {
+export default function BoardCommentWriteUI(props) {
   return (
     <Wrapper>
       <CommentIcon></CommentIcon>
       <span>댓글</span>
       <InputWrapper>
-        <Input placeholder="작성자" />
-        <Input placeholder="비밀번호" />
+        <Input placeholder="작성자" onChange={props.onChangeMyWiter} />
+        <Input
+          placeholder="비밀번호"
+          type="password"
+          onChange={props.onChangeMyPassword}
+        />
+        <Star onChange={props.onChangeStar} />
       </InputWrapper>
       <ContentsWrapper>
-        <Contents placeholder="개인정보를 공유 및 요청하거나, 명예 훼손, 무단 광고, 불법 정보 유포시 모니터링 후 삭제될 수 있으며, 이에 대한 민형사상 책임은 게시자에게 있습니다."></Contents>
+        <Contents
+          placeholder="개인정보를 공유 및 요청하거나, 명예 훼손, 무단 광고, 불법 정보 유포시 모니터링 후 삭제될 수 있으며, 이에 대한 민형사상 책임은 게시자에게 있습니다."
+          onChange={props.onChangeMyContents}
+        ></Contents>
         <Line></Line>
         <BottomWrapper>
           <BottomLength>0/100</BottomLength>
-          <BottomButton>등록하기</BottomButton>
+          <BottomButton onClick={props.onClickWrite}>등록하기</BottomButton>
         </BottomWrapper>
       </ContentsWrapper>
     </Wrapper>
