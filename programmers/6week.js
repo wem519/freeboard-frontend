10d46b6774de78844dde8d71290ae99fc6ee538a
wem519 <wem519@gmail.com>
2021-10-05 12:13:53 +0900

    6주차 1일 알고리즘

---
+++ programmers/6week.js
@@ -0,0 +1,16 @@
+//포켓몬
+
+function solution(nums) {
+  // var answer = 0;
+  const pocket = [];
+  //포켓몬들을 담는 배열
+  for (let i = 0; i < nums.length; i++) {
+    if (pocket.includes(nums[i]) === false && pocket.length < nums.length / 2) {
+      // console.log(nums[i])
+      pocket.push(nums[i]);
+    }
+    console.log(pocket, i);
+  }
+  return pocket.length;
+}
+ㅊㅇ;
