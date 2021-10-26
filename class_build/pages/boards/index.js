92df6914ebb044eeb71b74165b985a819fcf7165
wem519 <wem519@gmail.com>
2021-10-26 13:31:47 +0900

    ssr

--- class_build/pages/boards/index.js
+++ class_build/pages/boards/index.js
@@ -1,3 +1,15 @@
+import Head from "next/head"
+
 export default function BoardPage() {
-  return <div>게시판입니다!</div>;
+  return (
+  <>
+  <Head>
+    <meta property="og:title" content="나의 사이트 게시판 페이지"/>
+    <meta property="og:description" content="환영합니다!!"/>
+    <meta propety="og:image" content="https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg"/>
+  </Head>
+  <div>게시판입니다!</div>
+  </>
+  );
 }
+
